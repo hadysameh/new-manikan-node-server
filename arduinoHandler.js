7c5a7db70b693423488689ef7b97512d46a21a7b
hady <hadysameh007@gmail.com>
2025-04-18 23:24:38 +0200

    managed to get the changed bones

--- arduinoHandler.js
+++ arduinoHandler.js
@@ -11,16 +11,88 @@ const rightParser = new ReadlineParser();
 
 let codesToEmit = {};
 let voltsToEmit = {};
-let bonesAnglesToEmit = {};
+let lastSentBonesAngles = {};
 
 setInterval(() => {
   // console.log({ codesToEmit });
   global.io.emit('arduinoData', codesToEmit);
   global.io.emit('volts', voltsToEmit);
-  global.io.emit('angles', bonesAnglesToEmit);
+  global.io.emit('angles', lastSentBonesAngles);
 }, 500);
 
-const getBonesCodes = (calibratedBonesAngles) => {};
+function euclideanDistance(newBoneAnglesMap, oldBoneAnglesMap) {
+  const ADiff = Math.pow(
+    (newBoneAnglesMap?.A || 0) - (oldBoneAnglesMap?.A || 0),
+    2
+  );
+  const BDiff = Math.pow(
+    (newBoneAnglesMap?.B || 0) - (oldBoneAnglesMap?.B || 0),
+    2
+  );
+  const cDiff = Math.pow(
+    (newBoneAnglesMap?.C || 0) - (oldBoneAnglesMap?.C || 0),
+    2
+  );
+
+  return Math.sqrt(ADiff + BDiff + cDiff);
+}
+
+/**
+ *
+ * @param {object} calibratedBonesAngles
+ * Example
+ * input
+ * {'Ctrl_UpLeg_FK_Right.B': 20, 'Ctrl_UpLeg_FK_Right.C': 20, 'Ctrl_UpLeg_FK_Right.A': 20,}
+ *
+ * output
+ * { "Ctrl_UpLeg_FK_Right": {  "B": 20, "C": 20, "A": 20 }}
+ *
+ * @returns
+ */
+const getBoneAnglesMap = (calibratedBonesAngles) => {
+  const result = {};
+  for (const [key, value] of Object.entries(calibratedBonesAngles)) {
+    const [prefix, suffix] = key.split('.');
+    if (!result[prefix]) {
+      result[prefix] = {};
+    }
+    result[prefix][suffix] = value;
+  }
+  return result;
+};
+
+const getChangedBonesNames = (calibratedBonesAxesAngles) => {
+  const changeThershold = 1;
+  const newBonesAnglesMap = getBoneAnglesMap(calibratedBonesAxesAngles);
+  const oldBonesAnglesMap = getBoneAnglesMap(lastSentBonesAngles);
+  const changedBonesAngles = [];
+
+  for (const boneName in newBonesAnglesMap) {
+    const oldBoneAxesAngles = oldBonesAnglesMap[boneName];
+    const newBoneAxesAngles = newBonesAnglesMap[boneName];
+    const incomingChange = euclideanDistance(
+      newBoneAxesAngles,
+      oldBoneAxesAngles
+    );
+
+    if (incomingChange > changeThershold) {
+      changedBonesAngles.push(boneName);
+    }
+  }
+
+  return changedBonesAngles;
+};
+
+const getBonesCodes = (changedBonesAngles) => {
+  // steps
+  // 0- get bones maps
+  // 1- get the bones that has changes
+  // 2- get codes for changes bones
+
+  for (const boneNameWithRobotAxis in changedBonesAngles) {
+    const robotAxisAngle = changedBonesAngles[boneNameWithRobotAxis];
+  }
+};
 
 function calibrateBonesVoltages(bonesNamesWithAxis) {
   const calibratedVoltages = {};
@@ -108,9 +180,12 @@ const handleArduinoData = (data, sideName) => {
     }
 
     const calibratedBonesAxesVolts = calibrateBonesVoltages(recievedBonesVolts);
-    let bonesAxesAngles = getBonesAngles(calibratedBonesAxesVolts);
+    let calibratedBonesAxesAngles = getBonesAngles(calibratedBonesAxesVolts);
+    const changedBonesNames = getChangedBonesNames(calibratedBonesAxesAngles);
+    console.log({ changedBonesNames });
+    Object.assign(lastSentBonesAngles, calibratedBonesAxesAngles);
 
-    const codesForThreeAxesBones = getBonesCodes(bonesAxesAngles);
+    // const codesToEmit = getBonesCodes(changedBonesNames);
     // const codesForOneAxisBones = getCodesForOneAxisBones(bonesAxesAngles);
 
     // const newCodesToEmit = {
